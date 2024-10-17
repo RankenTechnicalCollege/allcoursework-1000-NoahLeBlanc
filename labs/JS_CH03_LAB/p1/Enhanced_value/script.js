6ae475203175fccae42d103d02f7aa3bb2625983
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-10-17 11:36:15 -0500

    Fin enhanced rate calc

--- labs/JS_CH03_LAB/p1/Enhanced_value/script.js
+++ labs/JS_CH03_LAB/p1/Enhanced_value/script.js
@@ -21,14 +21,28 @@ do {
 }
   while(isNaN(years));
 
-let futureValue = investment; //makes futureValue the same as investment value
+const months = years * 12 //converts # of years into months
+const monthly_rate = rate/12
+let futureValue_years = investment; //makes futureValue the same as investment value
 for (let i = 1; i <= years; i++){ //i increase by 1 if less than or equal to value of years.
-  futureValue += futureValue * rate / 100;//makes futureValue = itself times rate divided by 100
+  futureValue_years += futureValue_years * rate / 100;//makes futureValue = itself times rate divided by 100
 }
+let futureValue_months = investment; //makes futureValue the same as investment value
+for (let i = 1; i <= months; i++){ //i increase by 1 if less than or equal to value of years.
+  futureValue_months += futureValue_months * monthly_rate / 100;//makes futureValue = itself times rate divided by 100
+}
+
+
+//Doc write for the Yearly value
 document.write(`<h1>The Future Value Calculator</h1>`)
+document.write(`<h2>The Future Value with yearly interest</h2>`)
 document.write(`<p><label>Investment amount: </label>${investment}</p>`);
 document.write(`<p><label>Interest rate: </label>${rate}</p>`);
 document.write(`<p><label>Years: </label>${years}</p>`);
-document.write(`<p><label>Future Value: </label>${futureValue.toFixed(2)}</p>`);
-
-
+document.write(`<p><label>Future Value: </label>${futureValue_years.toFixed(2)}</p>`);
+//Doc write for the monthly value
+document.write(`<h2>The Future Value with Monthly interest intrest</h2>`)
+document.write(`<p><label>Investment amount: </label>${investment}</p>`);
+document.write(`<p><label>Interest rate: </label>${rate}</p>`);
+document.write(`<p><label>Years: </label>${years} (Months:${months})</p>`);
+document.write(`<p><label>Future Value: </label>${futureValue_months.toFixed(2)}</p>`);
