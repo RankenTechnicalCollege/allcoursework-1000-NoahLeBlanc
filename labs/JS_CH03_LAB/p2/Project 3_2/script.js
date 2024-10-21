c0b60a7204655207b81b50a2d7ee2d5a608868ca
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-10-21 08:30:20 -0500

    Finished 3_2

--- labs/JS_CH03_LAB/p2/Project 3_2/script.js
+++ labs/JS_CH03_LAB/p2/Project 3_2/script.js
@@ -1,8 +1,15 @@
 document.getElementById('btnClick').onclick = () => {
-  let total = 0
-  let Price = parseFloat(document.getElementById("purchasePrice").value);
-  if(Price <= 25.00){
-    total = Price + 1.50
+  let price = parseFloat(document.getElementById("purchasePrice").value);
+  let total = 0;
+  let fee = 0;
+  if(price <= 25.00){
+    fee = 1.50;
+    total = price + fee;
   }
+  else if(price > 25.00){
+    fee = price * 10 / 100;
+    total = price + fee;
+  };
+  alert(`Price: $${price.toFixed(2)} + Fee: $${fee.toFixed(2)}\n= Total Price: $${total.toFixed(2)}`)
 }
 
