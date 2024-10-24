cca9dafdaad2a16d02286db320a4b79729f61b1e
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-10-24 10:38:23 -0500

    Commit 2
    More progress made on the future val calculator .
    created data validation.;

--- labs/JS_CH04_LAB/Future Value/script.js
+++ labs/JS_CH04_LAB/Future Value/script.js
@@ -1,13 +1,35 @@
 document.getElementById('btnClick').onclick = () => {
+  /*Declares Vars the stores HTML values*/ 
   let totalInvestment = parseFloat(document.getElementById("totalInvestment").value);
+  console.log(totalInvestment)
   let annualInterestRate = parseFloat(document.getElementById("annualInterestRate").value);
+  console.log(annualInterestRate)
   let numOfYears = parseFloat(document.getElementById("numOfYears").value);
+  console.log(numOfYears)
+
+  /*Handles Broad Errors*/
+  if(totalInvestment <= 0 || annualInterestRate <= 0 || numOfYears <= 0){
+    alert("Inputs needs to be greater than 0");
+  }
+  else if(isNaN(totalInvestment) || isNaN(annualInterestRate) || isNaN(numOfYears)){
+    alert("Inputs needs to be a number");
+  }
+
+  /*Handles Specific Errors*/
+  else if(totalInvestment > 100000){
+    alert("Investment Needs to be Less than or equal to 100,000.");
+  }
+  else if(annualInterestRate > 15){
+    alert("Annual Interest Rate Needs to be Less than or equal to 15.");
+  }
+  /*Calls function and uses previously defines vars as inputs for params.*/
   futureValue = calculateFV(totalInvestment, annualInterestRate, numOfYears)
   document.getElementById("futureValue").value = futureValue;
 }
 
 function calculateFV(investment, rate, years){
-  futureValue = investment
-  futureValue += futureValue * rate / 100
-  return futureValue;
-}
+  for(i=1; i<= years * 12; i++) {
+    futureValue = futureValue + investment
+      (1 + rate * 12);
+  return futureValue.toFixed(2);
+}}
