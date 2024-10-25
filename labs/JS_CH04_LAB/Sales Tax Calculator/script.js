5d43e4fff16c5e3d6dd8326ffb887d670e5f766e
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-10-25 08:48:05 -0500

    10/25/2024	JS Ch4	Sales Tax Calculator		C2
    Completed Calculation work for Sales Tax Calculator.
    Added console log for easy debug.

--- labs/JS_CH04_LAB/Sales Tax Calculator/script.js
+++ labs/JS_CH04_LAB/Sales Tax Calculator/script.js
@@ -4,8 +4,8 @@ document.getElementById('btnClick').onclick = () => {
   let taxRate = parseFloat(document.getElementById("taxRate").value);
 
   /*------------------------------------Logs input for debug---------------------------------------*/
-  console.log(subtotal);
-  console.log(taxRate);
+  console.log(`subtotal: ${subtotal}`);
+  console.log(`Tax Rate: ${taxRate}`);
 
   /*------------------------------------Handles Data Validation------------------------------------*/
   if(isNaN(subtotal) || isNaN(taxRate)){
@@ -16,9 +16,11 @@ document.getElementById('btnClick').onclick = () => {
   }
   /*------------------------------------Inject Outputs into HTML------------------------------------*/
   else{
-  calculateTax(subtotal, taxRate);
-  document.getElementById("salesTax").value = salesTax;
-  document.getElementById("total").value = total;
+    returnArray = calculateTax(subtotal, taxRate);
+    console.log("Gathering Array Data")
+    console.log("Retuning Array Values to Index page")
+    document.getElementById("salesTax").value = returnArray[0];
+    document.getElementById("total").value = returnArray[1];
   };
   /*-------------------------------Sets cursor back to the first text box---------------------------*/
   document.getElementById('subtotal').focus();
@@ -32,7 +34,23 @@ function calculateTax(subtotal, taxRate){/*Local Scope vars, won't cause issues 
   }
   else if(taxRate < 0 && taxRate > 12){
     alert("Tax Rate must be Greater than 0\nand Less than 12")
-  };
+  }
   /*Completes Function and returns Value*/
+  else{
+    console.log("Data Validation Success!\nStarting Calculation")
+    taxRate = taxRate/100
+    console.log(`TaxRate / 100 = ${taxRate}`)
+    salesTax = subtotal * taxRate
+    console.log(`subtotal * taxRate =  ${salesTax}(Unrounded)`)
+    total = subtotal + salesTax
+    console.log(`subtotal + SalesTax =  ${total}(Unrounded)`)
+  };
+  console.log("Calculations Complete\nRounding Output")
+  salesTax = salesTax.toFixed(2)
+  console.log(`salesTax Rounded to 2nd digit = ${salesTax}`)
+  total = total.toFixed(2)
+  console.log(`total Rounded to 2nd digit = ${total}`)
+  let returnArray = [salesTax, total];
+  return returnArray;
 };
 
