347a2b2a651f804cd45e58c391f751bf24ed8d25
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-10-25 11:19:40 -0500

    10/25/2024	JS Ch4	Income Tax Calculator		C2
    Added:
    Input gatherer (HTML input into js)
    Calculation Handler(Calculates data into desired output)
    Output Injector (for HTML Output)
    Data validator(Check if input is correct)

---
+++ labs/JS_CH04_LAB/Income Tax Calculator/script.js
@@ -0,0 +1,57 @@
+document.getElementById('btnClick').onclick = () => {
+/*------------------------------Gather Inputs from HTML------------------------------------*/
+  let taxableIncome = parseFloat(document.getElementById("taxableIncome").value);
+/*------------------------------------Handles Data Validation------------------------------------*/
+  if(taxableIncome < 0){
+    alert("Income must at least 0\nNegative numbers are not allowed")
+  }
+  else if (isNaN(taxableIncome)){
+    alert("Please enter a number")
+  }
+  else{
+    taxOwed = calculateIncomeTax(taxableIncome)
+/*------------------------------------Inject Outputs into HTML------------------------------------*/
+    document.getElementById("incomeTax").value = taxOwed;
+  }
+};
+
+
+
+/*------------------------------------Handles Calculations------------------------------------*/
+function calculateIncomeTax(taxableIncome){
+  /*----------------For Taxable Income over $0 but not over $9,875-----------------------*/
+  if(taxableIncome > 0 && taxableIncome <= 9875){
+    taxableIncome = 0 + ((taxableIncome - 0)/10)
+    return taxableIncome
+  }
+  /*----------------For Taxable Income over $9,875 but not over $40,125------------------*/
+  else if(taxableIncome > 9875 && taxableIncome <= 40125){
+    taxableIncome = 987.50 + ((taxableIncome - 9875)*.12)
+    return taxableIncome
+  }
+  /*----------------For Taxable Income over $40,125 but not over $85,525-----------------*/
+  else if(taxableIncome > 40125 && taxableIncome <= 85525){
+    taxableIncome = 4617.50 + ((taxableIncome - 40125)*.22)
+    return taxableIncome.toFixed(2)
+  }
+  /*----------------For Taxable Income over $85,525 but not over $163,300----------------*/
+  else if(taxableIncome > 85525 && taxableIncome <= 163300){
+    taxableIncome = 14605.50 + ((taxableIncome - 85525)*.24)
+    return taxableIncome
+  }
+  /*----------------For Taxable Income over $163,300 but not over $207,350---------------*/
+  else if(taxableIncome > 163300 && taxableIncome <= 207350){
+    taxableIncome = 33271.50 + ((taxableIncome - 163300)*.32)
+    return taxableIncome
+  }
+  /*----------------For Taxable Income over $207,350 but not over $518,400---------------*/
+  else if(taxableIncome > 207350 && taxableIncome <= 518400){
+    taxableIncome = 47367.50 + ((taxableIncome - 207350)*.35)
+    return taxableIncome.toFixed(2)
+  }
+  /*---------------------------For Taxable Income over $518,400--------------------------*/
+  else{
+    taxableIncome = 156235.00 + ((taxableIncome - 518400)*.37)
+    return taxableIncome
+  };
+};
