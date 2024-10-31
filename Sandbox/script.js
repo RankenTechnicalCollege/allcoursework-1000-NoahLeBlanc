26cf7ecc24a3c5456b4f435f1b9f5701948242fe
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-10-31 11:31:28 -0500

    Finished Sandbox Project
    Thanks to the help from Mr. G I completed this mini project.
    Note:
    I failed to realize how the querySelectorAll works. It's not an array creator, it creates objects. which are key value pairs.
    so in order to properly add up the values of the inputs we need to iterate through every "key" adding their values together one by one.

--- Sandbox/script.js
+++ Sandbox/script.js
@@ -1,14 +1,20 @@
 document.getElementById('btclck').onclick = () => {
-  const inputs = document.querySelectorAll("input[type='number']");
+  /*------------------------------------Logs input for debug---------------------------------------*/
+  const days = document.querySelectorAll("input[class='days']");
+  const perHour = document.getElementById("perHour").value;
+  /*------------------------------------Adds Key value pair values together------------------------*/
   let hoursWorked = 0;
-
   //Loop through each input to get the value
-  inputs.forEach(input => {
+  days.forEach(input => {
     const value = parseFloat(input.value) || 0;
     hoursWorked += value;
   });
+  /*------------------------------------Inject Outputs into HTML------------------------------------*/
+  document.getElementById('output').value = `$${calculate(hoursWorked, perHour).toFixed(2)}`;
+}
 
-  //const hoursWorked = document.querySelectorAll("input").value; 
-  console.log(hoursWorked);
-  document.getElementById('output').value = hoursWorked;
+/*------------------------------------Function Calculates output---------------------------------*/
+function calculate(h, $){
+  cashEarned =  h * $
+  return cashEarned
 }
