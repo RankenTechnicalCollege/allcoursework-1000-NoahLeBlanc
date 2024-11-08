6c06a4d50b92e4d67580f8c63205cc620ecf8e45
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-11-08 10:38:22 -0600

    Finished Email list

--- book files/js/exercises/ch06/email_list/email_list.js
+++ book files/js/exercises/ch06/email_list/email_list.js
@@ -7,32 +7,44 @@ document.addEventListener("DOMContentLoaded", () => {
         const email1 = $("#email_1");
         const email2 = $("#email_2");
         const firstName = $("#first_name");
+        const email1Span = $("#email_1_span");
+        const email2Span = $("#email_2_span");
+        const firstNameSpan = $("#first_name_span");
     
         // create an error message and set it to an empty string
         let errorMessage = "";
 
         // check user entries - add text to error message if invalid
         if (email1.value == "") { 
-            errorMessage += "First email is required.\n";
+            email1Span.textContent = "First email is required.";
+        }else if(email1.value != ""){
+            email1Span.textContent = "*";
         }
     
         if (email2.value == "") { 
-            errorMessage += "Second email is required.\n";
+            email2Span.textContent = "Second email is required.";
+        }else if(email2.value != ""){
+            email1Span.textContent = "*";
         }
     
+    
         if (email1.value != email2.value) { 
-            errorMessage += "Both emails must match.\n";
+            email2Span.textContent = "Both emails must match.";
+        }else if(email1.value == email2.value && email2.value != ""){
+            email2Span.textContent = "*";
         }
     
         if (firstName.value == "") {
-            errorMessage += "First name is required.\n";
+            firstNameSpan.textContent = "First name is required.";
+        } else if(firstName.value != ""){
+            firstNameSpan.textContent = "*"
         }
     
         // submit the form if error message is an empty string
-        if (errorMessage == "") {
+        if (email1Span.textContent == "*" && email2Span.textContent == "*" && firstNameSpan.textContent == "*") {
             $("#email_form").submit();
         } else {
-            alert(errorMessage);            
+            console.log("User has not fixed mistakes")
         }
     });
 
