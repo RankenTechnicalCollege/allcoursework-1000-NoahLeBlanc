67587ad52ab51cc6c85e07bfc70c1c0d4d09b058
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-11-08 09:30:04 -0600

    Finished FAQ exercise

--- book files/js/exercises/ch06/faqs/faqs.js
+++ book files/js/exercises/ch06/faqs/faqs.js
@@ -2,23 +2,25 @@
 
 // the event handler for the click event of each h2 element
 const toggle = evt => {
-    const h2Element = evt.currentTarget;                 // get the clicked h2 element
+    const aElement = evt.currentTarget;                  //Gets the clicked a element
+    const h2Element = aElement.parentElement;            // get the parent h2 element
     const divElement = h2Element.nextElementSibling;     // get h2's sibling div
 
+    //handles the css
     h2Element.classList.toggle("minus");
     divElement.classList.toggle("open");
 
-    // cancel default action of h2 tag's <a> tag
+    evt.preventDefualt();
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    // get the h2 tags
-    const h2Elements = faqs.querySelectorAll("#faqs h2");
+document.addEventListener("DOMContentLoaded", () => { //makes sure the page loads and runs anon function
+    //get the h2's and the a's out of the h2's
+    const aElements = faqs.querySelectorAll("#faqs h2 a");
     
     // attach event handler for each h2 tag	    
-    for (let h2Element of h2Elements) {
-        h2Element.addEventListener("click", toggle);
+    for (let aElement of aElements) {
+        aElement.addEventListener("click", toggle);
     }
     // set focus on first h2 tag's <a> tag
-    h2Elements[0].firstChild.focus();       
+    aElements[0].firstChild.focus();       
 });
