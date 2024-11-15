732e9a9268e37764a70b4c15892f8ffed4a17db1
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-11-15 09:53:29 -0600

    Finished Ch 6 lab temp

--- book files/js/exercises/ch07/slide_show/slide_show.js
+++ book files/js/exercises/ch07/slide_show/slide_show.js
@@ -1,23 +1,27 @@
 "use strict";
 
 const $ = selector => document.querySelector(selector);
-    
+//Declare array and other vars
 const imageCache = [];
 let imageCounter = 0;
 let timer = null;
 let image = null;
-
+let stop = false
 const mainImage = $("#main_image");   // the img element for the show
 const caption = $("#caption");        // the h2 element for the caption
 
+//Handles slide show
 const runSlideShow = function() {
+    if(stop){
     imageCounter = (imageCounter + 1) % imageCache.length;
     image = imageCache[imageCounter];
+    //assigns mainImage to prev image with and object of src
     mainImage.src = image.src;
     mainImage.alt = image.alt;
     caption.textContent = image.alt;
+    };
 };
-         
+//performs code when HTML is loaded
 document.addEventListener("DOMContentLoaded", () => {
     const links = $("#image_list").querySelectorAll("a");
     
@@ -34,9 +38,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // attach start and pause event handlers
     $("#start").addEventListener("click", () => {
-
+        //disables start when clicked
+        const start = $("#start")
+        start.disabled = true;
+        pause.disabled = false;
+        stop=true;
+        runSlideShow(); 
+        const slideInterval = setInterval(runSlideShow, 2000)
     });
     $("#pause").addEventListener("click", () => {
-
+        const pause = $("#pause");
+        pause.disabled = false;
+        start.disable = false;
+        stop = false;
+        
     });
 });
--- labs/JS_CH06_LAB/Convert Temperatures/script.js
+++ labs/JS_CH06_LAB/Convert Temperatures/script.js
@@ -1,29 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
   const $ = selector => document.querySelector(selector);
-  //Triggers when FTC is selected
+  let inputLabel = $("#inputlabel")
+  let outputLabel = $("#outputlabel")
+  
   $('#FTC').onclick = () => {
-    $("#inputlabel").innerHTML = "Enter f degree"
+    inputLabel.textContent = "Enter F degrees"
+    outputLabel.textContent = "Degrees Celsius"
   }
   //Triggers when CTF is selected
-  $('#FTC').checked = () => {
-    let label = $("#inputlabel").innerHTML
-    label.innerHTML = "xyz"
+  $('#CTF').onclick = () => {
+    inputLabel.textContent = "Enter C degrees"
+    outputLabel.textContent = "Degrees Fahrenheit"
   }
 
 
   //Triggers when Convert Button is clicked.
   document.getElementById('btnClick').onclick = () => {
     //Gather's input
-    const input = $("#input").value
-    //Handles Data
+    let input = parseFloat($("#input").value)
     //Handles Data and Calculation
-    if ($("#FTC").checked && input != ""){
+    if(isNaN(input)){
+      $("#output").value = "Error"
+      let footer = $("footer")
+      footer.innerHTML = ""
+      let pElement = document.createElement("p");
+      let ScoresText = document.createTextNode("You must enter a valid number for degrees")
+      pElement.appendChild(ScoresText)
+      footer.appendChild(pElement)
+    }
+    else if ($("#FTC").checked && input != ""){
       output = Math.round((input - 32) * 5/9)
+      let footer = $("footer")
+      footer.innerHTML = ""
       $("#output").value = output
     }else if ($("#CTF").checked && input != ""){
+      let footer = $("footer")
+      footer.innerHTML = ""
       output = Math.round((input * (9/5) + 32));
+      //Injects Output 
       $("#output").value = output
-    //Injects Output 
     }
   }
 })
