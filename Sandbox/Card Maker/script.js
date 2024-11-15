0fbe5e14c8cefb2c4c3fb6d50e3e4ed6fd21fb3a
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-11-15 11:50:15 -0600

    Card maker

---
+++ Sandbox/Card Maker/script.js
@@ -0,0 +1,11 @@
+const $ = selector => document.querySelector(selector);
+//Get main area to work in
+const main = $("main")
+const section = document.createElement("section")
+const div1 = document.createElement("div")
+const div2 = document.createElement("div")
+const h2 = document.createElement("h2")
+const p = document.createElement("p")
+
+const card = section.appendChild(div1)
+console.log(card.parentNode)
