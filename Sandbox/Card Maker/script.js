8702211f22305e2d865cb90e97ea6b0eb84e00a5
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-11-20 11:37:50 -0600

    FIXED ALL ERRORS WITH DISPLAY

--- Sandbox/Card Maker/script.js
+++ Sandbox/Card Maker/script.js
@@ -1,5 +1,6 @@
 const $ = selector => document.querySelector(selector);
-//Creates Elements to be used
+/*--------------------------------------------Handles Building the Card------------------------------------------------*/
+/*---------Creates Elements to be used----*/
 const section = document.createElement("section")
 const div1 = document.createElement("div")
 const div2 = document.createElement("div")
@@ -8,46 +9,101 @@ const p = document.createElement("p")
 const ul = document.createElement("ul")
 const ol = document.createElement("ol")
 const li = document.createElement("li")
-//Builds Card and puts it into the HTML
+/*----------Builds the cards base---------*/
 section.appendChild(div1)
 div1.appendChild(h2)
 div1.appendChild(div2)
-//Adds Base Classes to Elements
+/*-------Adds Base Classes to the card-----*/
 section.classList.add("card") //Makes Card Dark
 div1.classList.add("card-body")
 div2.classList.add("card-text")
 h2.classList.add("card-title", "pb-2")
-/*--------------------Toggles Display for the input forms--------------*/
+/*-----------------------------------------Handles Content type Input Visuals-----------------------------------------------------*/
+/*-------Makes Shorthand for Toggles-----*/
+const pToggle = () => {pInputForm.classList.toggle("open")}
+const ulToggle = () =>{ulInputForm.classList.toggle("open")}
+const olToggle = () =>{olInputForm.classList.toggle("open")}
+/*-Shorthand Check if classList contains "open"-*/
+const pContains = () =>{
+  if(pInputForm.classList.contains("open")){
+    return true
+  }
+  else{
+    return false
+  }
+}
+const ulContains = () =>{
+  if(ulInputForm.classList.contains("open")){
+    return true
+  }
+  else{
+    return false
+  }
+}
+const olContains = () =>{
+  if(olInputForm.classList.contains("open")){
+    return true
+  }
+  else{
+    return false
+  }
+}
+/*-------Handles on refresh Scenarios-----*/
 document.addEventListener("DOMContentLoaded", () => {
   if($("#p").checked){
-    pInputForm.classList.toggle("open")
+    pToggle()
+  }
+  if($("#ul").checked){
+    ulToggle()
+  }
+  if($("#ol").checked){
+    olToggle()
   }
 })
-  //Changes pInputForm depending on checked values
+/*changes Input Display depending on checked value*/
 function changeDisplay(){
   if($("#p").checked){
-    pInputForm.classList.toggle("open")
-  }
-  else if($("#ul").checked){
-    if(pInputForm.classList.toggle("open")){
-      pInputForm.classList.toggle("open")
-    }
-    else{
-      
-    }
-    
-  }
-  else if($("#ol").checked){
-    if(pInputForm.classList.toggle("open")){
-      pInputForm.classList.toggle("open")
+      if(ulContains()){//if ul display is Visible:
+        ulToggle()//Turns off ul  input display
+        pToggle()//Turns on p input display
       }
-    else{
-      console.log("It's already closed")
+      else if(olContains()){ 
+        olToggle()
+        pToggle()
+      }
+      else{
+        pToggle()
+      }
+    }
+  if($("#ul").checked){
+      if(olContains()){
+        olToggle()
+        ulToggle()
+      }
+      else if(pContains()){
+        pToggle()
+        ulToggle()
+      }
+      else{
+        ulToggle()
+      }
+    }
+  if($("#ol").checked){
+      if(pContains()){
+        pToggle()
+        olToggle()
+        }
+      else if(ulContains()){
+        ulToggle()
+        olToggle()
+        }
+      else{
+        olToggle()
       }
     }
 }
 
-/*--------------------------Changes Card based on input--------------*/
+/*---------------------------------------------------Changes Card based on input--------------------------------------------------*/
 
 
 
