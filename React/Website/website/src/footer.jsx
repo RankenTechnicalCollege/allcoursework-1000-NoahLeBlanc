444b24b8d9f5c6d0ba2ad823a5f2d92e0198acb1
Noah LeBlanc <noah_leblanc@insideranken.org>
2024-12-04 11:20:36 -0600

    React Ranken Website Commit 1

---
+++ React/Website/website/src/footer.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+function Footer() {
+  //What is displayed to the Home page
+  return (
+    <>
+    </>
+  )
+}
+
+export default Footer
--- React/Website/website/src/main.jsx
+++ React/Website/website/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Header from './header.jsx'
+import Middle from './middle.jsx'
+import Footer from './footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Middle />
+    <Footer />
   </StrictMode>,
 )
